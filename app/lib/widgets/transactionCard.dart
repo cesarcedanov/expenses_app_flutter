ea8429f8160671bc8c38a99b326bc1ba4d82b733
CesarCedanoV <CesarCedanoV@gmail.com>
2019-08-13 21:26:37 -0400

    Refactor the Chart

--- app/lib/widgets/transactionCard.dart
+++ app/lib/widgets/transactionCard.dart
@@ -11,41 +11,65 @@ class TransactionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.white,
-      child: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 2)),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              '${transaction.currency} ${transaction.amount.toStringAsFixed(2)}',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Colors.redAccent),
+      elevation: 5,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: EdgeInsets.all(5),
+            child: FittedBox(
+              child: Text(
+                  '${transaction.currency} ${transaction.amount.toStringAsFixed(2)}'),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 5),
-                child: Text(
-                  transaction.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-              ),
-              Text(
-                DateFormat.yMMMMEEEEd().add_jm().format(transaction.date),
-                style: TextStyle(fontSize: 12, color: Colors.grey),
-              ),
-            ],
-          )
-        ],
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(transaction.date),
+        ),
       ),
     );
+
+    // return Card(
+    //   color: Colors.white,
+    //   child: Row(
+    //     children: <Widget>[
+    //       Container(
+    //         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+    //         decoration: BoxDecoration(
+    //             border: Border.all(color: Colors.black, width: 2)),
+    //         padding: EdgeInsets.all(10),
+    //         child: Text(
+    //           '${transaction.currency} ${transaction.amount.toStringAsFixed(2)}',
+    //           style: TextStyle(
+    //               fontWeight: FontWeight.bold,
+    //               fontSize: 18,
+    //               color: Colors.redAccent),
+    //         ),
+    //       ),
+    //       Column(
+    //         crossAxisAlignment: CrossAxisAlignment.start,
+    //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //         children: <Widget>[
+    //           Padding(
+    //             padding: EdgeInsets.only(bottom: 5),
+    //             child: Text(
+    //               transaction.title,
+    //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    //             ),
+    //           ),
+    //           Text(
+    //             DateFormat.yMMMMEEEEd().add_jm().format(transaction.date),
+    //             style: TextStyle(fontSize: 12, color: Colors.grey),
+    //           ),
+    //         ],
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 }
